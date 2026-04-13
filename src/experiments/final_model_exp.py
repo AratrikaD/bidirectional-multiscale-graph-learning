8b77711e6981972e5780108fffbef911826cb9ef
Aratrika Das <A.Das-12@student.tudelft.nl>
2026-04-13 23:22:49 +0200

    fix

--- src/experiments/final_model_exp.py
+++ src/experiments/final_model_exp.py
@@ -32,5 +32,5 @@ def run_final_exp(data_path, hyperparameters):
     
     print("start training")
     train_sliding_window(model=model, optimizer=optimizer, criterion=criterion, transactions=transactions, node_features=node_features, edge_index=edge_index,
-                        window_months=61, epochs=100, batch_size=64, base_patience=30)
+                        window_months=61, epochs=1, batch_size=128, base_patience=30)
     
